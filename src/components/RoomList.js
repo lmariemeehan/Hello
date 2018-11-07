66dc3b8371098db5388971ad0eb7f963f4edc0f1
Lola <lmariemeehan@gmail.com>
2018-11-07 14:30:46 -0800

    Fixed key error by adding it to the list item

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 
 class RoomList extends Component {
 	constructor(props) {
-		super(props); 
+		super(props);
 		this.state = {
 		rooms: []
 		};
 
-	this.roomsRef = this.props.firebase.database().ref('rooms');	
+	this.roomsRef = this.props.firebase.database().ref('rooms');
 	}
 
 	componentDidMount() {
@@ -21,7 +21,7 @@ class RoomList extends Component {
 
   render() {
     return (
-    	<ul>{this.state.rooms.map((room) => (<li>{room.name}</li>))}</ul>
+    	<ul>{this.state.rooms.map((room) => (<li key={room.name}>{room.name}</li>))}</ul>
     )
   }
 }
