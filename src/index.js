3b3bda685b939be564d82015bf959c59299ad9d7
Lola <lmariemeehan@gmail.com>
2020-04-09 17:52:35 -0700

    Cleaned up indexjs page

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
- ReactDOM.render(<App />
-
-  ,document.getElementById('root'));
+ ReactDOM.render(<App /> , document.getElementById('root'));
 
 serviceWorker.unregister();
