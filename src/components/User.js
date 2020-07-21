f2483c94e03bf7b2c04082196a0b0c3c7ce350ec
Lola <lmariemeehan@gmail.com>
2020-07-20 23:56:11 -0700

    Fixed firebase issue. It should have been down in the body of the index html file not in the head

--- src/components/User.js
+++ src/components/User.js
@@ -19,31 +19,31 @@ class User extends Component {
   }
 
   render() {
-  return (
-    <div className="container-fluid mt-4">
-      <h1 className="align-middle text-warning"><span className="user-icon align-middle"><ion-icon name="contact"></ion-icon></span>Hello... </h1>
-      <div className="user-info">
-        <div className="btn-group dropdown">
-          <button 
-            type="button" 
-            className="btn btn-transparent text-white font-weight-bold dropdown-toggle" 
-            data-toggle="dropdown" 
-            aria-haspopup="true" 
-            aria-expanded="false">
-              {this.props.user ? this.props.user.displayName : "GUEST" }
-          </button>
+    return (
+      <div className="container-fluid mt-4">
+        <h1 className="align-middle text-warning"><span className="user-icon align-middle"><ion-icon name="contact"></ion-icon></span>Hello... </h1>
+        <div className="user-info">
+          <div className="btn-group dropdown">
+            <button 
+              type="button" 
+              className="btn btn-transparent text-white font-weight-bold dropdown-toggle" 
+              data-toggle="dropdown" 
+              aria-haspopup="true" 
+              aria-expanded="false">
+                {this.props.user ? this.props.user.displayName : "GUEST" }
+            </button>
 
-          <div className="dropdown-menu py-2 text-muted">
-            {this.props.user ?
-              <button className="dropdown-item" onClick={this.signOut.bind(this)}>Logout</button> :
-              <button className="dropdown-item" onClick={this.signIn.bind(this)}>Login</button>
-            }
+            <div className="dropdown-menu py-2 text-muted">
+              {this.props.user ?
+                <button className="dropdown-item" onClick={this.signOut.bind(this)}>Logout</button> :
+                <button className="dropdown-item" onClick={this.signIn.bind(this)}>Login</button>
+              }
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-  );
+    );
   }
 }
 
